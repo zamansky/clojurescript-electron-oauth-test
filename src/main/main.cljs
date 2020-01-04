25f6e937940c8b0cba4ab58693ab614312fdf438
Mike Zamansky <zamansky@gmail.com>
2020-01-04 16:07:51 -0500

    have it working but need to be able to select users each time

--- src/main/main.cljs
+++ src/main/main.cljs
@@ -8,7 +8,7 @@
 (def main-window (atom nil))
 (def auth-window (atom nil))
 
-(def auth-url "https://github.com/login/oauth/authorize?login=\"\"&client_id=3056f749c2e91ce4d780&scope=admin:org delete_repo")
+(def auth-url "https://github.com/login/oauth/authorize?client_id=3056f749c2e91ce4d780&scope=admin:org delete_repo")
 
 (def id "3056f749c2e91ce4d780")
 (def secret  "a94c6cac3ba9a2f13b4c112a762169bc18bfd7a1")
@@ -39,6 +39,7 @@
           ;;       (prn "HELLO")
           (prn (str (keys response)) )
           (prn  "THE RESPONSE: " (:body response))
+          (.loadURL @auth-window (str "file://" js/__dirname "/public/index.html"))
           ;;(GET url {:params {:client_id id :client_secret secret :code code}
           ;;          :handler handler
           ;;          })
@@ -46,17 +47,26 @@
 
 
 (defn init-browser []
-(reset! auth-window (BrowserWindow.
-                     (clj->js {:width 1200
-                               :height 1200
-                               ;;"node-integration" false
-                               ;;"web-security" false
-                               })))
+  (reset! auth-window (BrowserWindow.
+                       (clj->js {:width 1200
+                                 :height 1200
+                                 ;;"node-integration" false
+                                 ;;"web-security" false
+                                 })))
 
-;;(.show @auth-window)
-(.loadURL @auth-window auth-url)
-(.on @auth-window "closed" #(reset! main-window nil))
-(.webContents.on @auth-window  "will-navigate" #(get-token %2)))
+  ;;(.show @auth-window)
+  (.catch (.loadURL @auth-window auth-url)
+          (fn [x]
+            (prn (get-token (.toString  (js->clj x))))
+            ;;(.log js/console  x))
+            ))
+  
+  ;;(.webContents.on @auth-window "did-fail-load" #(.log js/console (.sender.history %)))
+  (.on @auth-window "load" #(prn %1))
+  (.on @auth-window "closed" #(reset! main-window nil))
+  (.webContents.on @auth-window  "will-navigate" #(do
+                                                    (prn  %1 %2  )
+                                                    ( get-token %2))))
 
 ;; webView.getSettings().setJavaScriptEnabled(true);
 ;; authWindow.webContents.on('will-navigate', function (event, newUrl) {
